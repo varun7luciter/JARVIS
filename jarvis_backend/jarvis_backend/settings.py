da649f0fd509721d2776c36589e4996f46f4d22f
varun7lucifer <varun10102004@gmail.com>
2026-09-10 14:11:47 +0530

    Update settings.py

--- jarvis_backend/jarvis_backend/settings.py
+++ jarvis_backend/jarvis_backend/settings.py
@@ -21,9 +21,13 @@ SECRET_KEY = os.getenv(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG=False
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = "*"
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".onrender.com",
+]
 
 
 # Application definition
